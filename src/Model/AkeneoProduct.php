6d2fe26950a850880542dbda8eae4fa6b47be35c
Patryk <patryk.nietyksza@gmail.com>
2023-11-18 18:50:42 +0100

    AkeneoProduct and BaselinkerProduct setters and getters

--- src/Model/AkeneoProduct.php
+++ src/Model/AkeneoProduct.php
@@ -4,32 +4,147 @@ namespace App\Model;
 
 class AkeneoProduct
 {
-    public $uuid;
-    public $identifier;
-    public $enabled;
-    public $manufacturer;
-    public $ean;
-    public $erpName;
-    public $tradeName;
-    public $nameRequiredByLaw;
-    public $created;
-    public $updated;
-    public $associations;
+    protected $uuid;
+    protected $identifier;
+    protected $enabled;
+    protected $manufacturer;
+    protected $ean;
+    protected $erpName;
+    protected $tradeName;
+    protected $nameRequiredByLaw;
+    protected $created;
+    protected $updated;
+    protected $associations;
 
-    public function __construct($data)
+    public function __construct(array $data)
     {
-        $this->uuid = $data['uuid'];
-        $this->identifier = $data['identifier'];
-        $this->enabled = $data['enabled'];
-        $this->manufacturer = $data['values']['manufacturer'][0]['data'];
-        $this->ean = $data['values']['ean'][0]['data'];
-        if (isset($data['values']['erp_name'][0]['data'])) {
-            $this->erpName = $data['values']['erp_name'][0]['data'];
-        }
-        $this->tradeName = $data['values']['Trade_name'][0]['data'];
-        $this->nameRequiredByLaw = $data['values']['Name_required_by_law'][0]['data'];
-        $this->created = $data['created'];
-        $this->updated = $data['updated'];
-        $this->associations = $data['associations'];
+        $this->setters($data);
+    }
+
+    protected function setters(array $data): void
+    {
+        $this->uuid = $data['uuid'] ?? null;
+        $this->identifier = $data['identifier'] ?? null;
+        $this->enabled = $data['enabled'] ?? null;
+        $this->manufacturer = $data['values']['manufacturer'][0]['data'] ?? null;
+        $this->ean = $data['values']['ean'][0]['data'] ?? null;
+        $this->erpName = $data['values']['erp_name'][0]['data'] ?? null;
+        $this->tradeName = $data['values']['Trade_name'][0]['data'] ?? null;
+        $this->nameRequiredByLaw = $data['values']['Name_required_by_law'][0]['data'] ?? null;
+        $this->created = $data['created'] ?? null;
+        $this->updated = $data['updated'] ?? null;
+        $this->associations = $data['associations'] ?? null;
+    }
+
+    // Getters
+    public function getUuid(): ?string
+    {
+        return $this->uuid;
+    }
+
+    public function getIdentifier(): ?string
+    {
+        return $this->identifier;
+    }
+
+    public function getEnabled(): ?bool
+    {
+        return $this->enabled;
+    }
+
+    public function getManufacturer(): ?string
+    {
+        return $this->manufacturer;
+    }
+
+    public function getEan(): ?string
+    {
+        return $this->ean;
+    }
+
+    public function getErpName(): ?string
+    {
+        return $this->erpName;
+    }
+
+    public function getTradeName(): ?string
+    {
+        return $this->tradeName;
+    }
+
+    public function getNameRequiredByLaw(): ?string
+    {
+        return $this->nameRequiredByLaw;
+    }
+
+    public function getCreated(): ?string
+    {
+        return $this->created;
+    }
+
+    public function getUpdated(): ?string
+    {
+        return $this->updated;
+    }
+
+    public function getAssociations(): ?array
+    {
+        return $this->associations;
+    }
+
+    // Setters
+    public function setUuid(?string $uuid): void
+    {
+        $this->uuid = $uuid;
+    }
+
+    public function setIdentifier(?string $identifier): void
+    {
+        $this->identifier = $identifier;
+    }
+
+    public function setEnabled(?bool $enabled): void
+    {
+        $this->enabled = $enabled;
+    }
+
+    public function setManufacturer(?string $manufacturer): void
+    {
+        $this->manufacturer = $manufacturer;
+    }
+
+    public function setEan(?string $ean): void
+    {
+        $this->ean = $ean;
+    }
+
+    public function setErpName(?string $erpName): void
+    {
+        $this->erpName = $erpName;
+    }
+
+    public function setTradeName(?string $tradeName): void
+    {
+        $this->tradeName = $tradeName;
+    }
+
+    public function setNameRequiredByLaw(?string $nameRequiredByLaw): void
+    {
+        $this->nameRequiredByLaw = $nameRequiredByLaw;
+    }
+
+    public function setCreated(?string $created): void
+    {
+        $this->created = $created;
+    }
+
+    public function setUpdated(?string $updated): void
+    {
+        $this->updated = $updated;
+    }
+
+    public function setAssociations(?array $associations): void
+    {
+        $this->associations = $associations;
     }
 }
--- src/Model/BaselinkerProduct.php
+++ src/Model/BaselinkerProduct.php
@@ -4,27 +4,32 @@ namespace App\Model;
 
 class BaselinkerProduct
 {
-    public $storage_id;
-    public $product_id;
-    public $ean;
-    public $sku;
-    public $name;
-    public $quantity;
-    public $price_brutto;
-    public $price_wholesale_netto;
-    public $tax_rate;
-    public $weight;
-    public $description;
-    public $description_extra1;
-    public $description_extra2;
-    public $description_extra3;
-    public $description_extra4;
-    public $man_name;
-    public $category_id;
-    public $images = [];
-    public $features = [];
+    protected $storage_id;
+    protected $product_id;
+    protected $ean;
+    protected $sku;
+    protected $name;
+    protected $quantity;
+    protected $price_brutto;
+    protected $price_wholesale_netto;
+    protected $tax_rate;
+    protected $weight;
+    protected $description;
+    protected $description_extra1;
+    protected $description_extra2;
+    protected $description_extra3;
+    protected $description_extra4;
+    protected $man_name;
+    protected $category_id;
+    protected $images = [];
+    protected $features = [];
 
     public function __construct(array $data)
+    {
+        $this->setters($data);
+    }
+
+    protected function setters(array $data): void
     {
         $this->storage_id = $data['storage_id'] ?? null;
         $this->product_id = $data['product_id'] ?? null;
@@ -52,4 +57,196 @@ class BaselinkerProduct
             $this->features = $data['features'];
         }
     }
+
+        // Getters
+        public function getStorageId(): ?string
+        {
+            return $this->storage_id;
+        }
+    
+        public function getProductId(): ?string
+        {
+            return $this->product_id;
+        }
+    
+        public function getEan(): ?string
+        {
+            return $this->ean;
+        }
+    
+        public function getSku(): ?string
+        {
+            return $this->sku;
+        }
+    
+        public function getName(): ?string
+        {
+            return $this->name;
+        }
+    
+        public function getQuantity(): ?int
+        {
+            return $this->quantity;
+        }
+    
+        public function getPriceBrutto(): ?float
+        {
+            return $this->price_brutto;
+        }
+    
+        public function getPriceWholesaleNetto(): ?float
+        {
+            return $this->price_wholesale_netto;
+        }
+    
+        public function getTaxRate(): ?float
+        {
+            return $this->tax_rate;
+        }
+    
+        public function getWeight(): ?float
+        {
+            return $this->weight;
+        }
+    
+        public function getDescription(): ?string
+        {
+            return $this->description;
+        }
+    
+        public function getDescriptionExtra1(): ?string
+        {
+            return $this->description_extra1;
+        }
+    
+        public function getDescriptionExtra2(): ?string
+        {
+            return $this->description_extra2;
+        }
+    
+        public function getDescriptionExtra3(): ?string
+        {
+            return $this->description_extra3;
+        }
+    
+        public function getDescriptionExtra4(): ?string
+        {
+            return $this->description_extra4;
+        }
+    
+        public function getManName(): ?string
+        {
+            return $this->man_name;
+        }
+    
+        public function getCategoryId(): ?int
+        {
+            return $this->category_id;
+        }
+    
+        public function getImages(): array
+        {
+            return $this->images;
+        }
+    
+        public function getFeatures(): array
+        {
+            return $this->features;
+        }
+    
+        // Setters
+        public function setStorageId(?string $storageId): void
+        {
+            $this->storage_id = $storageId;
+        }
+    
+        public function setProductId(?string $productId): void
+        {
+            $this->product_id = $productId;
+        }
+    
+        public function setEan(?string $ean): void
+        {
+            $this->ean = $ean;
+        }
+    
+        public function setSku(?string $sku): void
+        {
+            $this->sku = $sku;
+        }
+    
+        public function setName(?string $name): void
+        {
+            $this->name = $name;
+        }
+    
+        public function setQuantity(?int $quantity): void
+        {
+            $this->quantity = $quantity;
+        }
+    
+        public function setPriceBrutto(?float $priceBrutto): void
+        {
+            $this->price_brutto = $priceBrutto;
+        }
+    
+        public function setPriceWholesaleNetto(?float $priceWholesaleNetto): void
+        {
+            $this->price_wholesale_netto = $priceWholesaleNetto;
+        }
+    
+        public function setTaxRate(?float $taxRate): void
+        {
+            $this->tax_rate = $taxRate;
+        }
+    
+        public function setWeight(?float $weight): void
+        {
+            $this->weight = $weight;
+        }
+    
+        public function setDescription(?string $description): void
+        {
+            $this->description = $description;
+        }
+    
+        public function setDescriptionExtra1(?string $descriptionExtra1): void
+        {
+            $this->description_extra1 = $descriptionExtra1;
+        }
+    
+        public function setDescriptionExtra2(?string $descriptionExtra2): void
+        {
+            $this->description_extra2 = $descriptionExtra2;
+        }
+    
+        public function setDescriptionExtra3(?string $descriptionExtra3): void
+        {
+            $this->description_extra3 = $descriptionExtra3;
+        }
+    
+        public function setDescriptionExtra4(?string $descriptionExtra4): void
+        {
+            $this->description_extra4 = $descriptionExtra4;
+        }
+    
+        public function setManName(?string $manName): void
+        {
+            $this->man_name = $manName;
+        }
+    
+        public function setCategoryId(?int $categoryId): void
+        {
+            $this->category_id = $categoryId;
+        }
+    
+        public function setImages(array $images): void
+        {
+            $this->images = $images;
+        }
+    
+        public function setFeatures(array $features): void
+        {
+            $this->features = $features;
+        }
 }
